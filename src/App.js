44e3fc3cfebfa3f47455285f7ea31a3b2cc28015
dieulx <dieu.lx@hsc.com.vn>
2022-08-05 16:43:50 +0700

    init firebase;

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import { initializeApp } from "firebase/app";
+import Dashboard from './components/Dashboard';
 
+const firebaseConfig = {
+  apiKey: "AIzaSyDbR_bR7_eR3j864xuRRJCOJVMLytAs7oE",
+  authDomain: "vocab-parse.firebaseapp.com",
+  projectId: "vocab-parse",
+  storageBucket: "vocab-parse.appspot.com",
+  messagingSenderId: "879757099295",
+  appId: "1:879757099295:web:95f03fb9384c4ff176b02a"
+};
+initializeApp(firebaseConfig);
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Dashboard/>
   );
 }
 
